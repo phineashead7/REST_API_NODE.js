46b666a970eebb1dcfb890be8cfb8cd8981c9483
phineashead7 <56085220+phineashead7@users.noreply.github.com>
2019-10-02 21:42:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
 const userRoutes = require('./api/routes/user');
 const personRoutes = require('./api/routes/person');
 
@@ -23,24 +21,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => { 
-    req.header('Access-Control-Allow-Origin', '*');
-    req.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accpet, Authorization'
-    );
-    if(req.method === 'OPTIONS'){
-        res.header(
-            'Access-Control-Allow-Methods',
-            'PUT, POST, PATCH, DELETE, GET'
-        );
-        return res.status(200).json({});
-    }
-    next();
-});
-
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
 app.use('/user', userRoutes);
 app.use('/person', personRoutes);
 
